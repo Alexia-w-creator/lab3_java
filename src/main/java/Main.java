5f633fbd0d463ca94eba5343971c184a959a7971
Alexia-w-creator <75364812+Alexia-w-creator@users.noreply.github.com>
2022-11-29 21:11:16 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,11 @@ public class Main {
         Timer time = new Timer(arr, list);
         System.out.println("Method\t"+"Count\t"+"ArrayList(ms)\t"+"LinkedList(ms)");
 
-        time.addTimerAp();
-        time.sizeTimerAp();
-        time.getTimerAp();
-        time.containsTimerAp();
-        time.setTimerAp();
-        time.deleteTimerAp();
+        time.addTimer();
+        time.sizeTimer();
+        time.getTimer();
+        time.containsTimer();
+        time.setTimer();
+        time.deleteTimer();
     }
 }
